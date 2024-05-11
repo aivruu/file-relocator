b8612287402ece6949ff45a5edaefdb8432ce046
Qekly <benjaalejandro206@gmail.com>
2024-05-11 15:14:03 -0400

    feat(docs): Define documentation for the packages

---
+++ src/main/java/me/qeklydev/relocator/io/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides utilities for I/O writing.
+ *
+ * @since 0.0.1
+ */
+package me.qeklydev.relocator.io;
---
+++ src/main/java/me/qeklydev/relocator/relocation/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides utilities for the files relocation.
+ *
+ * @since 0.0.1
+ */
+package me.qeklydev.relocator.relocation;
